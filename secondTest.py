ec58fbde5e69c9b2f23a37ec8a5655320996a655
Leo Donati <leo.donati@unice.fr>
2023-03-10 18:53:39 +0100

    changé la clé

--- secondTest.py
+++ secondTest.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = "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"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
